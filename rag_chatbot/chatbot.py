0d68d3822c63b5fb3146c3c2f18428f7fd1e466b
John Wendlandt <johnwendlandt@Johns-MacBook-Pro-59.local>
2025-04-14 15:46:46 -0400

    minor pinecone changes

--- rag_chatbot/chatbot.py
+++ rag_chatbot/chatbot.py
@@ -4,24 +4,28 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Pinecone
 from langchain.llms import HuggingFaceHub
 import pinecone
+# from pinecone.grpc import PineconeGRCP as Pinecone
 from dotenv import load_dotenv
 import os
 
 class ChatBot():
   load_dotenv()
-  loader = TextLoader('./nhl_stats_full.txt')
+  loader = TextLoader('../data/nhl_stats_full.txt')
   documents = loader.load()
-  text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=4)
+  text_splitter = CharacterTextSplitter(chunk_size=3100, chunk_overlap=30)
   docs = text_splitter.split_documents(documents)
 
   embeddings = HuggingFaceEmbeddings()
 
   pinecone.init(
-      api_key= os.getenv('PINECONE_API_KEY'),
-      environment='gcp-starter'
+    #   api_key= os.getenv('PINECONE_API_KEY'),
+      api_key='pcsk_2oxcYN_NPhzizrpYnXdAbvyAt38mF8iGZBNVLXRKqXhKjn94qWRUqEUAt3s8ZyetjnQTCr',
+      environment='us-east-1'
   )
 
-  index_name = "langchain-demo"
+  index_name = "llama-text-embed-v2-index"
+#   pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
+#   index = pc.index(host='https://llama-text-embed-v2-index-quskxg4.svc.aped-4627-b74a.pinecone.io/')
 
   if index_name not in pinecone.list_indexes():
     pinecone.create_index(name=index_name, metric="cosine", dimension=768)
@@ -31,7 +35,7 @@ class ChatBot():
 
   repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
   llm = HuggingFaceHub(
-      repo_id=repo_id, model_kwargs={"temperature": 0.8, "top_p": 0.8, "top_k": 50}, huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_KEY')
+      repo_id=repo_id, model_kwargs={"temperature": 0.8, "top_p": 0.8, "top_k": 50}, huggingfacehub_api_token='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'
   )
 
   from langchain import PromptTemplate
@@ -59,4 +63,8 @@ class ChatBot():
     | StrOutputParser() 
   )
 
-  if __name__ == "main.py": 
+if __name__ == "main.py": 
+        bot = ChatBot()
+        input = input("Ask me anything: ")
+        result = bot.rag_chain.invoke(input)
+        print(result)
