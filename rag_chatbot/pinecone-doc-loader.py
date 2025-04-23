f9ee5e82fdacb5dbeac3dbfb919649df0e13dfe4
Carl Gross <carlgross0123@gmail.com>
2025-04-23 12:28:58 -0400

    dataproc

--- rag_chatbot/pinecone-doc-loader.py
+++ rag_chatbot/pinecone-doc-loader.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import TextLoader
@@ -12,10 +14,47 @@ embeddings = OpenAIEmbeddings()
 
 vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
 
-loader = TextLoader("../data/nhl_stats_full.txt")
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=3100, chunk_overlap=30)
-docs = text_splitter.split_documents(documents)
+txt_dir = "../data/jsons"
+
+def check_metadata_size(metadatas):
+    for meta in metadatas:
+        meta_size = sys.getsizeof(str(meta))  # Get the size of metadata
+        if meta_size > 40960:
+            print(f"Metadata exceeds size limit: {meta_size} bytes")
+            return False  # Metadata size exceeds limit
+    return True
+
+def prune_metadata(metadata):
+    return {"source": metadata["source"]}  # Keep only the essential metadata
+
+for filename in os.listdir(txt_dir):
+    if filename.endswith('.txt'):
+        file_path = os.path.join(txt_dir, filename)
+        
+        # Load, split, and add to vector store
+        loader = TextLoader(file_path)
+        documents = loader.load()
+        
+        text_splitter = CharacterTextSplitter(chunk_size=3000, chunk_overlap=250)
+        docs = text_splitter.split_documents(documents)
+
+        texts = [doc.page_content for doc in docs]
+        metadatas = [{"source": filename} for _ in docs]
+
+        metadatas = [prune_metadata(meta) for meta in metadatas]
+        
+        # vectorstore.add_texts(texts=texts, metadatas=metadatas)
+
+        if check_metadata_size(metadatas):
+            vectorstore.add_texts(texts=texts, metadatas=metadatas)
+            print(f"Added documents from {filename} to vector store.")
+        else:
+            print(f"Skipping {filename} due to large metadata size.")
+
+# loader = TextLoader("../data/nhl_stats_full.txt")
+# documents = loader.load()
+# text_splitter = CharacterTextSplitter(chunk_size=3100, chunk_overlap=30)
+# docs = text_splitter.split_documents(documents)
 
 # vectorstore_from_docs = PineconeVectorStore.from_documents(
 #     docs,
@@ -25,7 +64,7 @@ docs = text_splitter.split_documents(documents)
 
 # vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
 
-vectorstore.add_documents(docs)
+# vectorstore.add_documents(docs)
 
 # texts = ["Tonight, I call on the Senate to: Pass the Freedom to Vote Act.", "ne of the most serious constitutional responsibilities a President has is nominating someone to serve on the United States Supreme Court.", "One of our nation’s top legal minds, who will continue Justice Breyer’s legacy of excellence."]
 
