bd278eff0ccd470c831b55e5f782e358dc2f7d29
Carl Gross <carlgross0123@gmail.com>
2025-05-04 13:36:12 -0400

    final

--- rag_chatbot/pinecone-doc-loader.py
+++ rag_chatbot/pinecone-doc-loader.py
@@ -10,10 +10,9 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain.embeddings import HuggingFaceEmbeddings
 
+os.environ['PINECONE_API_KEY'] = ""
 
-
-# index_name = "llama-text-embed-v2-index"
-index_name = "huggingface"
+index_name = ""
 # embeddings = OpenAIEmbeddings()
 embeddings = HuggingFaceEmbeddings()
 
@@ -37,78 +36,11 @@ for filename in os.listdir(txt_dir):
         file_path = os.path.join(txt_dir, filename)
         print(f"Processing: {filename}")
         
-        # Load, split, and add to vector store
         loader = TextLoader(file_path)
         documents = loader.load()
         
         text_splitter = CharacterTextSplitter(chunk_size=7000, chunk_overlap=100)
         docs = text_splitter.split_documents(documents)
-        # for doc in docs:
-        #     print(doc)
-        # texts = [doc.page_content for doc in docs]
-        # metadatas = [{"source": filename} for _ in docs]
-
-        # metadatas = [prune_metadata(meta) for meta in metadatas]
         
-        # vectorstore.add_texts(texts=texts, metadatas=metadatas)
-
-        # if check_metadata_size(metadatas):
-        #     vectorstore.add_texts(texts=texts, metadatas=metadatas)
-        #     print(f"Added documents from {filename} to vector store.")
-        # else:
-        #     print(f"Skipping {filename} due to large metadata size.")
-        # break
         vectorstore.add_documents(docs)
         print(f"Added documents from {filename} to vector store.")
-        # break
-        # break
-
-# loader = TextLoader("../data/nhl_stats_full.txt")
-# documents = loader.load()
-# text_splitter = CharacterTextSplitter(chunk_size=3100, chunk_overlap=30)
-# docs = text_splitter.split_documents(documents)
-# print(len(docs))
-
-# vectorstore_from_docs = PineconeVectorStore.from_documents(
-#     docs,
-#     index_name=index_name,
-#     embedding=embeddings
-# )
-
-# vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
-
-# vectorstore.add_documents(docs)
-
-# import json
-
-# # Path to your .txt file
-# file_path = '../data/nhl_stats_full.txt'
-
-# # Read and parse the JSON
-# with open(file_path, 'r') as file:
-#     data = json.load(file)
-
-# print(type(data))
-# print(json.dumps(data, indent=2)[:1000])
-
-# json_string = json.dumps(data)
-# # splitter = RecursiveJsonSplitter(max_chunk_size = 3000)
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size=3000,
-#     chunk_overlap=100
-# )
-# # json_chunks = splitter.split_json(json_data=json_string)
-# json_string = json.dumps(data)
-# docs = splitter.split_text(json_string)
-# # docs = splitter.split_text(json_data=json_string)
-
-# print(len(docs))
-# print(docs[0])
-
-# texts = ["Tonight, I call on the Senate to: Pass the Freedom to Vote Act.", "ne of the most serious constitutional responsibilities a President has is nominating someone to serve on the United States Supreme Court.", "One of our nation’s top legal minds, who will continue Justice Breyer’s legacy of excellence."]
-
-# vectorstore_from_texts = PineconeVectorStore.from_texts(
-#     texts,
-#     index_name=index_name,
-#     embedding=embeddings
-# )
